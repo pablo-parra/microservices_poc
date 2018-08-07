c75c51b1b68f63b507e690b6a72e28ea20122f1b
pabloparra <pabloparradominguez@gmail.com>
2018-08-07 14:33:29 +0200

    Added bootified configuration for the microservices. Removed sampleapp1b as now we can reproduce multiple instances using the bootified artifact

--- sampleapp1b/src/main/java/com/capgemini/sampleapp1/general/service/api/RestService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.sampleapp1.general.service.api;
-
-/**
- * A marker interface for REST services. REST service interfaces of components should extend this interface.
- *
- */
-public interface RestService {
-
-}
